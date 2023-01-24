bf9e6c9f97526aac01311ff1cb525013aacd182a
Jeffrey Luzon <iamjeffrey099@gmail.com>
2023-01-23 20:01:52 -0500

    added buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@
 function App() {
   return (
     <div className="App">
-      Hello world
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          
+          <button>DEL</button>
+        </div>
+        <div className="digits">
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+
+        </div>
+      </div>
     </div>
   );
 }
